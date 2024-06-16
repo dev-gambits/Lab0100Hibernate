3c79d3801be20264e7d3d91334a1707ffc57a49b
Emmanuel Nieto M <emmanuel.nieto.munoz@ciisa.cl>
2024-06-15 20:23:15 -0400

    Initial commit

---
+++ src/main/java/cl/sideralti/app/Lab0100HibernateApplication.java
@@ -0,0 +1,13 @@
+package cl.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0100HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0100HibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sideralti/app/Lab0100HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sideralti.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0100HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
